b270a05dee730287f6c763a9bb8158d1b97721b4
Luis Daniel Mendez Castellanos <syi216618@gmail.com>
2021-12-22 17:31:56 -0700

    Filters were added to the gallery interface adapting them to the parameters needed by the methods

--- frontend/src/views/Galeria.view.js
+++ frontend/src/views/Galeria.view.js
@@ -7,6 +7,8 @@ import {
 } from "../utils/blockchain_interaction";
 import { currencys } from "../utils/constraint";
 import { getNearContract, fromYoctoToNear } from "../utils/near_interaction";
+import filtroimg from '../assets/landingSlider/img/filtro.png'
+import countrys from '../utils/countrysList'
 
 
 function LightEcommerceA() {
@@ -14,71 +16,84 @@ function LightEcommerceA() {
     theme: "yellow",
     currency: currencys[parseInt(localStorage.getItem("blockchain"))],
     tokens: [],
-    page: parseInt( window.localStorage.getItem("page")),
+    page: parseInt(window.localStorage.getItem("page")),
     pag: window.localStorage.getItem("pagSale"),
     blockchain: localStorage.getItem("blockchain"),
     tokensPerPage: 10,
     tokensPerPageNear: 15,
   });
   const [counter, setcounter] = React.useState();
+  const [esconder, setesconder] = React.useState(true);
+  const [filtro, setfiltro] = React.useState({
+    culture: "null",
+    country: "null",
+    type: "null",
+    date: "null",
+    price: "null",
+  });
+  const [combo, setcombo] = React.useState(true);
 
-   
+  const modificarFiltro = (v) => {
+    setfiltro(c => ({ ...c, ...v }))
+  }
+
   React.useEffect(() => {
     (async () => {
       let toks, onSaleToks;
-      let arr=[];
-      
+      let arr = [];
+
       if (Landing.blockchain == "0") {
         //primero nos aseguramos de que la red de nuestro combo sea igual a la que esta en metamask
-          await syncNets();
-          //obtener cuantos tokens tiene el contrato
-          let totalSupply = await getContract().methods.totalSupply().call();
-          //obtener el numero de tokens a la venta
-          onSaleToks = await getContract().methods.nTokenOnSale.call().call();
+        await syncNets();
+        //obtener cuantos tokens tiene el contrato
+        let totalSupply = await getContract().methods.totalSupply().call();
+        //obtener el numero de tokens a la venta
+        onSaleToks = await getContract().methods.nTokenOnSale.call().call();
 
-            //indices del arreglo para la paginacion :::0*10=0 1*10=10  1*10=10 2*10=20
-          for(let i =Landing.page*10; i<(parseInt(Landing.page)+1)*Landing.tokensPerPage ; i++) {
-            //console.log("ini",Landing.page*10,"actual",i,"fin",(parseInt(Landing.page)+1)*Landing.tokensPerPage)
-            //obtiene la informacion de x token
-            let infoe  = await getContract().methods.getItemInfo(i).call();
-            //Valida si está a la venta
-             if(infoe[0].onSale){
-                  //agrega el token al arreglo para mostrar
-                  arr.push(infoe[0]);
-                  }
-                 
-           //Concadena el token encontrado con los tokens que ya se mostraron
-             setLanding({
-              ...Landing,
-              tokens: arr,
-              nPages: Math.ceil(arr.length / Landing.tokensPerPage),
-            });  
-
+        //indices del arreglo para la paginacion :::0*10=0 1*10=10  1*10=10 2*10=20
+        for (let i = Landing.page * 10; i < (parseInt(Landing.page) + 1) * Landing.tokensPerPage; i++) {
+          //console.log("ini",Landing.page*10,"actual",i,"fin",(parseInt(Landing.page)+1)*Landing.tokensPerPage)
+          //obtiene la informacion de x token
+          let infoe = await getContract().methods.getItemInfo(i).call();
+          //Valida si está a la venta
+          if (infoe[0].onSale) {
+            //agrega el token al arreglo para mostrar
+            arr.push(infoe[0]);
           }
-           
-     
+
+          //Concadena el token encontrado con los tokens que ya se mostraron
+          setLanding({
+            ...Landing,
+            tokens: arr,
+            nPages: Math.ceil(arr.length / Landing.tokensPerPage),
+          });
+
+        }
+
+
       } else {
         window.contr = await getNearContract();
-      
+
         //instanciar contracto
         let contract = await getNearContract();
         //console.log("Page",Landing.page)
         //obtener tokens a la venta
-       // console.log("Paasdsadfsdfdge",Landing.page*30,"edfew" ,Landing.tokensPerPageNear*(Landing.page+1))
+        // console.log("Paasdsadfsdfdge",Landing.page*30,"edfew" ,Landing.tokensPerPageNear*(Landing.page+1))
         let pag = await contract.get_pagination_onsale({
-          tokens: Landing.tokensPerPageNear})
-          
-        window.localStorage.setItem('pagSale',pag)
+          tokens: Landing.tokensPerPageNear
+        })
+
+        window.localStorage.setItem('pagSale', pag)
         let pagNumArr = pag
         toks = await contract.obtener_pagina_on_sale({
           tokens: Landing.tokensPerPageNear,
           _start_index: Landing.page,
-          status:"S",
-          _creatoraccount:"tes",
-          _owneraccount:"tes",
-          _minprice:0,
-          _maxprice:0,
-          _date:0,
+          status: "S",
+          _creatoraccount: "tes",
+          _owneraccount: "tes",
+          _minprice: 0,
+          _maxprice: 0,
+          _date: 0,
         });
         //obtener cuantos tokens estan a la venta
         onSaleToks = await contract.get_on_sale_toks();
@@ -112,7 +127,62 @@ function LightEcommerceA() {
   }, []);
   return (
     <section className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto">
+      <div className={"container px-5 py-4 mx-auto flex flex-wrap items-center " + (
+        esconder ? "" : "py-2"
+      )}>
+        <div className="fs-1 flex items-center" onClick={e => {
+          setesconder(v => !v);
+        }}>
+          <img src={filtroimg} className="logg mr-1" />
+          <b>Filtro</b>
+        </div>
+      </div>
+      <div className={"container py-5 px-5  mx-auto flex flex-wrap items-center " + (
+        esconder ? "" : "esconder"
+      )} >
+
+        <select className="p-2 lg:w-1/12 ml-2 bg-s1" onChange={e => {
+          setcombo(e.target.value == "Nuevos");
+        }}>
+          <option>Nuevos</option>
+          <option>Viejos</option>
+        </select>
+        {combo ? (
+          <>
+            <b className="ml-2" >Fecha:</b>
+            <input type="date" className="p-2 lg:w-1/18 ml-2 bg-s1" />
+          </>
+        ) : (
+          <>
+            <b className="ml-2" >Fecha inicial:</b>
+            <input type="date" className="p-2 lg:w-1/18 ml-2 bg-s1" />
+            <b className="ml-2" >Fecha final:</b>
+            <input type="date" className="p-2 lg:w-1/18 ml-2 bg-s1" />
+          </>
+        )}
+        <b className="ml-2" >Precio Minimo:</b>
+        <input type="number" className="p-2 lg:w-1/12 ml-2 bg-s1" min="0" step="0.1" />
+        <b className="ml-2" >Precio Maximo:</b>
+        <input type="number" className="p-2 lg:w-1/12 ml-2 bg-s1" min="0" step="0.1" />
+        <button className="ml-20 p-2 lg:w-1/12 ml-2 bg-s1"><b>Aplicar</b></button>
+
+        {/* <b className="ml-2" >País:</b>
+        <select className="p-2 lg:w-2/12 ml-2 bg-s1" onChange={e => {
+          modificarFiltro({ country: (e.target.value == "Todos los tokens" ? "null" : e.target.value) });
+        }}>
+          <option >
+            Todos los tokens
+          </option>
+          {
+            countrys.map(c => (
+              <option >
+                {c}
+              </option>
+            ))
+          }
+        </select> */}
+      </div>
+      <div className="container px-5 py-8 mx-auto">
         {/* Arroja un mensaje si no hay tokens disponibles en venta*/}
         {!Landing.tokens.length > 0 ? (
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
@@ -126,42 +196,42 @@ function LightEcommerceA() {
               const tokenData = JSON.parse(token.data);
               return (
                 <div className="lg:w-1/3 md:w-1/2 px-3 w my-" key={key}>
-                 {tokenData.image ?
-                  <a href={"/detail/" + token.tokenID}>
-                    <div className="token">
-                    <div className="block relative h-48 rounded overflow-hidden">
-                    
-                       <img
+                  {tokenData.image ?
+                    <a href={"/detail/" + token.tokenID}>
+                      <div className="token">
+                        <div className="block relative h-48 rounded overflow-hidden">
+
+                          <img
                             alt="ecommerce"
                             className="imgaa object-cover object-center w-full h-full block"
                             src={`https://ipfs.io/ipfs/${tokenData.image}`}
-                          /> 
-               
-                   
-                           
-                    </div>
-                    <div className="mt-4">
-                      <h2 className="ml-1 text-gray-900 title-font text-lg font-medium">
-                        {tokenData.title}
-                      </h2>
-                      <p className="mt-1 mb-4 ml-2">
-                        {"Tokenid: "+ token.tokenID }
-                        <br/>
-                        { "Owner: "+token.ownerId+"\n"}
-                        <br/>
-                        {Landing.blockchain==0 &&
-                            fromWEItoEth(token.price) + " " + Landing.currency}
+                          />
 
-                        {Landing.blockchain!=0 &&
+
+
+                        </div>
+                        <div className="mt-4">
+                          <h2 className="ml-1 text-gray-900 title-font text-lg font-medium">
+                            {tokenData.title}
+                          </h2>
+                          <p className="mt-1 mb-4 ml-2">
+                            {"Tokenid: " + token.tokenID}
+                            <br />
+                            {"Owner: " + token.ownerId + "\n"}
+                            <br />
+                            {Landing.blockchain == 0 &&
+                              fromWEItoEth(token.price) + " " + Landing.currency}
+
+                            {Landing.blockchain != 0 &&
                               fromYoctoToNear(token.price) + " " + Landing.currency}
-                      </p>
-                    </div>
-                    </div>
-                  </a>
-                  :
-                  <img 
-                     src={"https://media.giphy.com/media/tA4R6biK5nlBVXeR7w/giphy.gif"} 
-                     className="object-cover object-center w-full h-full block" />
+                          </p>
+                        </div>
+                      </div>
+                    </a>
+                    :
+                    <img
+                      src={"https://media.giphy.com/media/tA4R6biK5nlBVXeR7w/giphy.gif"}
+                      className="object-cover object-center w-full h-full block" />
 
                   }
                 </div>
@@ -172,7 +242,7 @@ function LightEcommerceA() {
           <nav
             className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
             aria-label="Pagination"
-          > 
+          >
             {Landing?.page != 0 && (
               <a
                 href="#"
@@ -197,20 +267,19 @@ function LightEcommerceA() {
             {[...Array(Landing?.nPages)].map((page, index) => {
               return (
                 <a
-                  
-                  className={`bg-white ${
-                    Landing.page == index
-                      ? "bg-yellow-100 border-yellow-500 text-yellow-600 hover:bg-yellow-200"
-                      : "border-gray-300 text-gray-500 hover:bg-gray-50"
-                  }  relative inline-flex items-center px-4 py-2 text-sm font-medium`}
+
+                  className={`bg-white ${Landing.page == index
+                    ? "bg-yellow-100 border-yellow-500 text-yellow-600 hover:bg-yellow-200"
+                    : "border-gray-300 text-gray-500 hover:bg-gray-50"
+                    }  relative inline-flex items-center px-4 py-2 text-sm font-medium`}
                   key={index}
                   onClick={async () => {
-                  //  await getPage(index);
-                    if(index == 0){
-                      window.localStorage.setItem("page",0)
+                    //  await getPage(index);
+                    if (index == 0) {
+                      window.localStorage.setItem("page", 0)
                     }
-                    else{
-                      window.localStorage.setItem("page",parseInt(Landing.pag.split(",")[index])+1);  
+                    else {
+                      window.localStorage.setItem("page", parseInt(Landing.pag.split(",")[index]) + 1);
                     }
                     //setcounter(Landing.tokens[Landing.tokens.length-1].tokenID +1)
 
