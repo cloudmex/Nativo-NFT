37fdba0c7b58a677fa0c0e8280f80a7e5b8065a6
Antonio Mora <dokxo96@gmail.com>
2021-12-28 15:46:42 -0700

    change the testnet subaccount
    this change was lost by the merge,cuz the merge order was not correct

--- frontend/src/utils/near_interaction.js
+++ frontend/src/utils/near_interaction.js
@@ -9,7 +9,7 @@ import {
 export const storage_byte_cost = 10000000000000000000;
 //export const contract_name = "nativo.near";
 //export const contract_name = "dokxo.testnet";
-export const contract_name =(process.env.REACT_APP_CONTRACT === undefined ? "sub.nativoapp.testnet" : process.env.REACT_APP_CONTRACT);
+export const contract_name =(process.env.REACT_APP_CONTRACT === undefined ? "nativo.dokxo.testnet" : process.env.REACT_APP_CONTRACT);
 export const config = {
   testnet: {
     networkId: "testnet",
