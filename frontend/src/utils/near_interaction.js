2e84d027a9778f033fa616f2ac0e3dce87818244
Luis Daniel Mendez Castellanos <syi216618@gmail.com>
2022-01-07 13:32:14 -0700

    The error in the search profile and in my NFTs was resolved, in addition to fixing visual problems present in my NFTs

--- frontend/src/utils/near_interaction.js
+++ frontend/src/utils/near_interaction.js
@@ -9,7 +9,7 @@ import {
 export const storage_byte_cost = 10000000000000000000;
 //export const contract_name = "nativo.near";
 //export const contract_name = "dokxo.testnet";
-export const contract_name =(process.env.REACT_APP_CONTRACT === undefined ? "dev-1641514596273-99685459701992" : process.env.REACT_APP_CONTRACT);
+export const contract_name =(process.env.REACT_APP_CONTRACT === undefined ? "nativov2.testnet" : process.env.REACT_APP_CONTRACT);
 export const config = {
   testnet: {
     networkId: "testnet",
